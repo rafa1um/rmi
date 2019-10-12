d9aad1e394afaa9a09a92826e9388ed0f50927da
rafa1um <rafael.rxp1@gmail.com>
2019-10-12 15:07:30 -0300

    window-test

--- blackboard/src/main/java/org/openjfx/MainApp.java
+++ blackboard/src/main/java/org/openjfx/MainApp.java
@@ -1,4 +1,43 @@
 package org.openjfx;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application implements EventHandler<ActionEvent> {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("BlackBoard");
+
+        button = new Button();
+        button.setText("Enviar");
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == button) {
+            System.out.println("Thank You, Friend!");
+        }
+    }
 }
